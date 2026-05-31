11f0658c8cfc342c273a685730caeecc0ca19256
itselectroz <harry.whittle@cwgsy.net>
2026-05-31 13:22:06 +0100

    Initial Commit

---
+++ TideWidget/TideWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TideWidgetApp.swift
+//  TideWidget
+//
+//  Created by Harry whittle on 31/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct TideWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
